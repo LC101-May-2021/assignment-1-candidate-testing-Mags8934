4e52e589d33ed8637083560d837d36b865852e67
Maggie Techner <>
2021-05-24 21:55:16 +0000

    Candidate Testing - Part 1 code

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,33 +3,41 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = input.question("What is your name? ");
+
+console.log("Hello "+ candidateName + "!");
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+
+let question = input.question("Who was the first American woman in space? ");
+let correctAnswer = "Sally Ride";
+let candidateAnswer = question;
 let questions;
-let correctAnswers;
-let candidateAnswers;
+let correctAnswers; 
+let candidateAnswers; 
 
-
-function askForName() {
+function askForName() { 
   // TODO 1.1b: Ask for candidate's name //
 
 }
-
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  
 }
+
+if (correctAnswer === candidateAnswer) {
+  console.log("Your answer is correct.");
+} else {
+    console.log("Your answer is incorrect.");
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
+ let grade;
   
 
   return grade;
@@ -42,6 +50,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
